d70805a6ebf74d9f090afa275cbc136c91a4df7e
isartise <izabela.grzelecka@o2.pl>
2022-09-26 20:35:30 +0200

    handling operations, refactoring methods

--- src/main/java/CalculatorService.java
+++ src/main/java/CalculatorService.java
@@ -1,119 +1,81 @@
 import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
 
 
 import static spark.Spark.*;
 public class CalculatorService {
     public static void main(String[] args) {
 
-        get("/hello", (req, res) -> "Hello World");
 
-        get("/hello/:name", (req, res) -> {
-            //return "Hello, "+ req.params(":name");
-            TwoOperandsRequest d = new TwoOperandsRequest();
+        post("/calculator/add", CalculatorService::handleAdd);
 
-            return new Gson().toJson(d);
-        });
-        post("/calculator/add", (req, res) -> {
-            TwoOperandsRequest requestObj = new Gson().fromJson(req.body(), TwoOperandsRequest.class);
-            MetricConverter metricConverter = new MetricConverter();
-            double operandA = 0;
-            if(requestObj.operandA.metric.equals("ft")){
-                operandA = metricConverter.FeetToMeters(requestObj.operandA.value);
-            }else if(requestObj.operandA.metric.equals("NM")){
-                operandA = metricConverter.NauticalMilesToMeters(requestObj.operandA.value);
-            }else if(requestObj.operandA.metric.equals("m"))
-                return requestObj.operandA.value;
-            double operandB = 0;
-            if(requestObj.operandB.metric.equals("ft")){
-                operandB = metricConverter.FeetToMeters(requestObj.operandB.value);
-            }else if(requestObj.operandB.metric.equals("NM")){
-                operandB = metricConverter.NauticalMilesToMeters(requestObj.operandB.value);
-            }else if(requestObj.operandB.metric.equals("m"))
-                return requestObj.operandB.value;
-            CalculatorEngine calculatorEngine = new CalculatorEngine();
-            double calculationResult = calculatorEngine.Add(operandA, operandB);
-            calculationResult = metricConverter.MetersToFeet(calculationResult);
-            double roundedResult = Math.round((calculationResult*100)/100);
-            ValueWithMetricRequest result = new ValueWithMetricRequest(roundedResult,requestObj.expectedResultMetric);
-            return new Gson().toJson(result);
+        post("/calculator/subtract", CalculatorService::handleSubtract);
 
-        });
-        post("/calculator/subtract", (req, res) -> {
-            TwoOperandsRequest requestObj = new Gson().fromJson(req.body(), TwoOperandsRequest.class);
-            MetricConverter metricConverter = new MetricConverter();
-            double operandA = 0;
-            if(requestObj.operandA.metric.equals("ft")){
-                operandA = metricConverter.FeetToMeters(requestObj.operandA.value);
-            }else if(requestObj.operandA.metric.equals("NM")){
-                operandA = metricConverter.NauticalMilesToMeters(requestObj.operandA.value);
-            }else if(requestObj.operandA.metric.equals("m"))
-                return requestObj.operandA.value;
-            double operandB = 0;
-            if(requestObj.operandB.metric.equals("ft")){
-                operandB = metricConverter.FeetToMeters(requestObj.operandB.value);
-            }else if(requestObj.operandB.metric.equals("NM")){
-                operandB = metricConverter.NauticalMilesToMeters(requestObj.operandB.value);
-            }else if(requestObj.operandB.metric.equals("m"))
-                return requestObj.operandB.value;
-            CalculatorEngine calculatorEngine = new CalculatorEngine();
-            double calculationResult = calculatorEngine.Subtract(operandA, operandB);
-            calculationResult = metricConverter.MetersToFeet(calculationResult);
-            double roundedResult = Math.round((calculationResult*100)/100);
-            ValueWithMetricRequest result = new ValueWithMetricRequest(roundedResult,requestObj.expectedResultMetric);
-            return new Gson().toJson(result);
+        post("/calculator/multiply", CalculatorService::handleMultiply);
 
+        post("/calculator/divide", CalculatorService::handleDivide);
 
-        });
-        post("/calculator/multiply", (req, res) -> {
-            TwoOperandsRequest requestObj = new Gson().fromJson(req.body(), TwoOperandsRequest.class);
-            MetricConverter metricConverter = new MetricConverter();
-            double operandA = 0;
-            if(requestObj.operandA.metric.equals("ft")){
-                operandA = metricConverter.FeetToMeters(requestObj.operandA.value);
-            }else if(requestObj.operandA.metric.equals("NM")){
-                operandA = metricConverter.NauticalMilesToMeters(requestObj.operandA.value);
-            }else if(requestObj.operandA.metric.equals("m"))
-                return requestObj.operandA.value;
-            double operandB = 0;
-            if(requestObj.operandB.metric.equals("ft")){
-                operandB = metricConverter.FeetToMeters(requestObj.operandB.value);
-            }else if(requestObj.operandB.metric.equals("NM")){
-                operandB = metricConverter.NauticalMilesToMeters(requestObj.operandB.value);
-            }else if(requestObj.operandB.metric.equals("m"))
-                return requestObj.operandB.value;
-            CalculatorEngine calculatorEngine = new CalculatorEngine();
-            double calculationResult = calculatorEngine.Multiply(operandA, operandB);
-            calculationResult = metricConverter.MetersToFeet(calculationResult);
-            double roundedResult = Math.round((calculationResult*100)/100);
-            ValueWithMetricRequest result = new ValueWithMetricRequest(roundedResult,requestObj.expectedResultMetric);
-            return new Gson().toJson(result);
+    }
 
-        });
-        post("/calculator/divide", (req, res) -> {
-            TwoOperandsRequest requestObj = new Gson().fromJson(req.body(), TwoOperandsRequest.class);
-            MetricConverter metricConverter = new MetricConverter();
-            double operandA = 0;
-            if(requestObj.operandA.metric.equals("ft")){
-                operandA = metricConverter.FeetToMeters(requestObj.operandA.value);
-            }else if(requestObj.operandA.metric.equals("NM")){
-                operandA = metricConverter.NauticalMilesToMeters(requestObj.operandA.value);
-            }else if(requestObj.operandA.metric.equals("m"))
-                return requestObj.operandA.value;
-            double operandB = 0;
-            if(requestObj.operandB.metric.equals("ft")){
-                operandB = metricConverter.FeetToMeters(requestObj.operandB.value);
-            }else if(requestObj.operandB.metric.equals("NM")){
-                operandB = metricConverter.NauticalMilesToMeters(requestObj.operandB.value);
-            }else if(requestObj.operandB.metric.equals("m"))
-                return requestObj.operandB.value;
-            CalculatorEngine calculatorEngine = new CalculatorEngine();
-            double calculationResult = calculatorEngine.Divide(operandA, operandB);
-            calculationResult = metricConverter.MetersToFeet(calculationResult);
-            double roundedResult = Math.round((calculationResult*100)/100);
-            ValueWithMetricRequest result = new ValueWithMetricRequest(roundedResult,requestObj.expectedResultMetric);
-            return new Gson().toJson(result);
+    public static String handleAdd(spark.Request req, spark.Response res){
+        TwoOperandsRequest requestObj = new Gson().fromJson(req.body(), TwoOperandsRequest.class);
 
-        });
+        MetricConverter metricConverter=new MetricConverter();
+        double operandA = metricConverter.convertToMeters(requestObj.operandA.value,requestObj.operandA.metric);
+        double operandB = metricConverter.convertToMeters(requestObj.operandB.value,requestObj.operandB.metric);
 
+        CalculatorEngine calculatorEngine = new CalculatorEngine();
+        double calculationResult = calculatorEngine.Add(operandA, operandB);
+
+        return prepareResponse(calculationResult, requestObj.expectedResultMetric);
+
+    }
+
+    public static String handleSubtract(spark.Request req, spark.Response res){
+        TwoOperandsRequest requestObj = new Gson().fromJson(req.body(), TwoOperandsRequest.class);
+
+        MetricConverter metricConverter=new MetricConverter();
+        double operandA = metricConverter.convertToMeters(requestObj.operandA.value,requestObj.operandA.metric);
+        double operandB = metricConverter.convertToMeters(requestObj.operandB.value,requestObj.operandB.metric);
+
+        CalculatorEngine calculatorEngine = new CalculatorEngine();
+        double calculationResult = calculatorEngine.Subtract(operandA, operandB);
+
+        return prepareResponse(calculationResult, requestObj.expectedResultMetric);
+
+    }
+    public static String handleDivide(spark.Request req, spark.Response res){
+        TwoOperandsRequest requestObj = new Gson().fromJson(req.body(), TwoOperandsRequest.class);
+
+        MetricConverter metricConverter=new MetricConverter();
+        double operandA = metricConverter.convertToMeters(requestObj.operandA.value,requestObj.operandA.metric);
+        double operandB = metricConverter.convertToMeters(requestObj.operandB.value,requestObj.operandB.metric);
+
+        CalculatorEngine calculatorEngine = new CalculatorEngine();
+        double calculationResult = calculatorEngine.Divide(operandA, operandB);
+
+        return prepareResponse(calculationResult, null);
+
+    }
+
+    public static String handleMultiply(spark.Request req, spark.Response res) {
+        TwoOperandsRequest requestObj = new Gson().fromJson(req.body(), TwoOperandsRequest.class);
+
+        MetricConverter metricConverter = new MetricConverter();
+        double operandA = metricConverter.convertToMeters(requestObj.operandA.value, requestObj.operandA.metric);
+        double operandB = requestObj.operandB.value;
+
+        CalculatorEngine calculatorEngine = new CalculatorEngine();
+        double calculationResult = calculatorEngine.Multiply(operandA, operandB);
+
+        calculationResult = metricConverter.convertFromMeters(calculationResult, requestObj.expectedResultMetric);
+
+        return prepareResponse(calculationResult, requestObj.expectedResultMetric);
+    }
+    private static String prepareResponse(double calculationResult, String expectedResultMetric){
+        double roundedResult = Math.round(calculationResult * 100) / 100.0;
+        ValueWithMetricRequest result = new ValueWithMetricRequest(roundedResult, expectedResultMetric);
+        return new Gson().toJson(result);
     }
 }
--- src/main/java/MetricConverter.java
+++ src/main/java/MetricConverter.java
@@ -23,4 +23,26 @@ public class MetricConverter {
     public double NauticalMilesToFeet(double nauticalMiles){
         return nauticalMiles * 6076.11672949;
     }
+    public double convertToMeters(double value,String metric) {
+        double valueInMeters = 0;
+        if(metric.equals("ft")){
+            valueInMeters = FeetToMeters(value);
+        }else if(metric.equals("NM")){
+            valueInMeters = NauticalMilesToMeters(value);
+        }else if(metric.equals("m")) {
+            valueInMeters = value;
+        }
+        return valueInMeters;
+    }
+    public double convertFromMeters(double valueInMeters,String destinationMetric) {
+        double valueInDestinationMetric = 0;
+        if(destinationMetric.equals("ft")){
+            valueInDestinationMetric = MetersToFeet(valueInMeters);
+        }else if(destinationMetric.equals("NM")){
+            valueInDestinationMetric = MetersToNauticalMiles(valueInMeters);
+        }else if(destinationMetric.equals("m")) {
+            valueInDestinationMetric = valueInMeters;
+        }
+        return valueInDestinationMetric;
+    }
 }
